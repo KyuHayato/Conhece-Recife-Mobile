a48f8fe53dfb8c72981bfcff9bbf9c758609fece
unknown <helderhenderson143@gmail.com>
2022-12-12 23:57:46 -0300

    create: in map view

--- frontend/src/views/ShowInMap.vue
+++ frontend/src/views/ShowInMap.vue
@@ -1,5 +1,5 @@
 <template>
-  <ion-header class="background-principal">
+  <ion-header class="header">
     <a href="/menu"
       ><ion-button class="background-none">
         <ion-img
@@ -7,16 +7,18 @@
           src="../../assets/seta.png "
         ></ion-img></ion-button
     ></a>
+    <h4>No mapa</h4>
   </ion-header>
 
   <ion-content>
-    <div style="border: 0; padding: 2rem">
+    <div>
       <iframe
         :src="place.imageSrc"
         width="100%"
         height="400"
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
+        style="border: 0; padding: 1rem"
       >
       </iframe>
 
@@ -26,6 +28,11 @@
           :src="place.imageUri[0]"
           alt="imagem do local"
         />
+        <span class="box_fast-titule-categories">{{ place.categoria[0] }}</span>
+
+        <div class="box_fast-description">
+          {{ place?.descricao }}
+        </div>
       </div>
     </div>
   </ion-content>
@@ -66,8 +73,9 @@ export default defineComponent({
         latitude: null,
         imageUri: "",
         imageSrc: "",
+        descricao: "",
+        categoria: "",
       },
-      key: process.env.API_KEY,
     };
   },
   created() {
@@ -79,8 +87,7 @@ export default defineComponent({
         .get(`http://localhost:3000/lugares/${this.$route.params.id}`)
         .then((result) => {
           this.place = result.data[0];
-          console.log(this.place);
-          this.place.imageSrc = `https://www.google.com/maps/embed/v1/place?key=AIzaSyAyoxyRxHbWpaUVYNILliXtClPDGk_NViU&q=${this.place.longitude},${this.place.latitude}&zoom=15`;
+          this.place.imageSrc = `https://www.google.com/maps/embed/v1/place?key=AIzaSyAyoxyRxHbWpaUVYNILliXtClPDGk_NViU&q=${this.place.longitude},${this.place.latitude}&zoom=16`;
         });
     },
   },
@@ -93,222 +100,70 @@ export default defineComponent({
   text-emphasis-color: #f8fafe;
 }
 
+.box_fast-description {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-style: italic;
+  font-weight: 400;
+  margin-right: 0.5rem;
+
+  position: relative;
+}
+
 .box_fast-detail {
-  margin-top: 1rem;
+  margin:0 1rem;
+
+  display: flex;
+  position: relative;
+
   background-color: orange;
   border-radius: 0.5rem;
+  border-top-right-radius: 2rem;
+  border-bottom-right-radius: 1rem;
   padding: 0.5rem;
 
   height: 10rem;
 }
 
+.box_fast-titule-categories {
+  position: absolute;
+
+  background-color: white;
+  border-radius: 0.5rem;
+
+  margin: 1rem 0 1rem 0.7rem;
+  padding: 0rem 0.7rem;
+
+  font-style: italic;
+
+  bottom: 0;
+}
+
 .box_fast-detail__img {
+  border-radius: 0.6rem;
+
+  margin-right: 1rem;
   width: 45%;
   height: 100%;
 }
 
-.ionCardMaster {
-  margin-top: -0.5em;
-  --background: #2f80ed;
-  padding-top: 0em;
-}
-
-.background-principal {
+.header {
+  color: white;
+  letter-spacing: .1rem;
   background-color: #2f80ed;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 
-.background-none {
-  background-color: none !important;
-  box-shadow: none !important;
-}
+.header h4 {
+  margin: 0;
+  margin-left: 2rem;
 
-swiper-slide {
-  box-sizing: border-box;
-
-  position: relative;
-
-  background-color: #e78500;
-  border: 5px solid #e78500;
-}
-
-.toolbar {
-  padding-bottom: 30px;
-  padding-top: 30px;
-}
-
-.imgGrid2 {
-  height: 25px;
-  width: 22px;
-  left: 168px;
-  top: 453px;
-  border-radius: 100%;
-  margin-top: 0.8em;
-  margin-right: 5px;
-  margin-left: 5px;
-}
-
-.textTitleMain {
-  font-family: "Roboto Condensed";
-  font-style: normal;
   font-weight: 700;
-  font-size: 30px;
-  line-height: 120%;
-  text-align: left;
-  /* identical to box height, or 36px */
-  margin-left: 0.5em;
-  letter-spacing: 2px;
-
-  color: #ffffff;
 }
 
-.imgGrid {
-  box-sizing: border-box;
-  border: 3px solid #db870f;
-  border-radius: 13px;
-  width: 75px;
-  height: 75px;
-
-  background: rgba(0, 0, 0, 0.2);
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-}
-
-.imgGrid3 {
-  box-sizing: border-box;
-  border: 3px solid #db870f;
-  border-radius: 13px;
-  width: 141px;
-  height: 108px;
-
-  background: rgba(0, 0, 0, 0.2);
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-}
-
-.imgCarrosel {
-  box-sizing: border-box;
-  border: 5px solid #db870f;
-  border-radius: 5px;
-}
-
-.imgCarrosel3 {
-  box-sizing: border-box;
-  border: 2px solid #db870f;
-  border-radius: 2px;
-}
-
-.cardSup {
-  background-color: #2f80ed;
-}
-
-.cardSup3 {
-  background-color: #2f80ed;
-}
-
-.seach {
-  margin-top: 1%;
-
-  color: hsl(0, 0%, 100%);
-  margin-bottom: 1px;
-  padding: 0%;
-
-  margin-left: 5%;
-  margin-right: 5%;
-
-  width: 90%;
-}
-
-.grid {
-  margin-top: -30px;
-  padding: 1px;
-}
-
-.cardBot {
-  color: #ffffff;
-}
-
-.colunaText {
-  width: 172px;
-  height: 27px;
-
-  /* Header/H2 */
-  margin-top: 10%;
-  text-align: left;
-  font-family: "Roboto";
-  font-style: normal;
-  font-weight: 700;
-  font-size: 18px;
-  line-height: 150%;
-  /* identical to box height, or 27px */
-
-  letter-spacing: 0.15px;
-
-  color: #130a72;
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-}
-
-.textTitleGrid {
-  font-family: "Open Sans";
-  font-style: normal;
-  font-weight: 800;
-  font-size: 14.6923px;
-  line-height: 20px;
-  text-align: center;
-  letter-spacing: 0.22806px;
-  text-align: left;
-  color: #ffffff;
-}
-
-.textSubtitleMain {
-  margin-top: 12px;
-  font-family: "Roboto";
-  font-style: normal;
-  font-weight: 800;
-  font-size: 14.6923px;
-  line-height: 20px;
-  text-align: center;
-  letter-spacing: 0.5px;
-  text-align: justify;
-  color: #ffffff;
-  line-height: 150%;
-}
-
-.textToolbar {
-  color: #ffffff;
-}
-
-.textSubtitleGrid {
-  text-align: justify;
-  color: #ffffff;
-  padding-left: 1;
-  margin-left: 1;
-}
-
-ion-col {
-  background-color: #525e04;
-  border: solid 1px #2f80ed;
-  padding: 2px;
-
-  text-align: center;
-
-  margin-top: 10%;
-
-  box-sizing: border-box;
-
-  width: 74px;
-  height: 74px;
-}
 </style>
